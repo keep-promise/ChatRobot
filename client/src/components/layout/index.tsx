8ed700fbe660f8f452fdfe6686c44d905609009d
keep_promise <1981672801@qq.com>
2023-11-06 00:21:22 +0800

    提交代码

--- client/src/components/layout/index.tsx
+++ client/src/components/layout/index.tsx
@@ -1,50 +1,50 @@
 import { MenuDataItem, ProLayout } from '@ant-design/pro-components'
 // import HeaderRender from '../HeaderRender';
+import React, { useEffect } from 'react';
 import { MenuProps } from 'antd';
 import { ChatsInfo } from '@/types';
-import React, { useEffect } from 'react';
 import { configStore } from '@/store';
 
 type Props = {
   menuExtraRender?: () => React.ReactNode
   route?: {
-    path: string
-    routes: Array<ChatsInfo>
+    path: string;
+    routes: Array<ChatsInfo>;
   }
   menuItemRender?: (
     item: MenuDataItem & {
-      isUrl: boolean
-      onClick: () => void
+      isUrl: boolean;
+      onClick: () => void;
     },
     defaultDom: React.ReactNode,
     menuProps: MenuProps | any
   ) => React.ReactNode | undefined
-  menuDataRender?: (menuData: MenuDataItem[]) => MenuDataItem[]
-  menuFooterRender?: (props?: any) => React.ReactNode
-  menuProps?: MenuProps
-  children?: React.ReactNode
+  menuDataRender?: (menuData: MenuDataItem[]) => MenuDataItem[];
+  menuFooterRender?: (props?: any) => React.ReactNode;
+  menuProps?: MenuProps;
+  children?: React.ReactNode;
 }
 
 function Layout(props: Props) {
-  const { menuExtraRender = () => <></>, menuItemRender = () => undefined } = props
+  const { menuExtraRender = () => <></>, menuItemRender = () => undefined } = props;
 
   const { website_logo, website_title, website_footer, website_description, website_keywords } =
-    configStore()
+    configStore();
 
   function createMetaElement(key: string, value: string) {
-    const isMeta = document.querySelector(`meta[name="${key}"]`)
+    const isMeta = document.querySelector(`meta[name="${key}"]`);
     if (!isMeta) {
-      const head = document.querySelector('head')
-      const meta = document.createElement('meta')
-      meta.name = key
-      meta.content = value
-      head?.appendChild(meta)
+      const head = document.querySelector('head');
+      const meta = document.createElement('meta');
+      meta.name = key;
+      meta.content = value;
+      head?.appendChild(meta);
     }
   }
 
   useEffect(() => {
-    createMetaElement('description', website_description)
-    createMetaElement('keywords', website_keywords)
+    createMetaElement('description', website_description);
+    createMetaElement('keywords', website_keywords);
   }, [])
 
   return (
--- client/src/routers/index.tsx
+++ client/src/routers/index.tsx
@@ -1,3 +1,17 @@
+import React from 'react';
+
+const ChatPage = React.lazy(() => import('@/pages/chat'))
+
 export const routers = [
-  
-]
+  {
+    id: 'ChatPage',
+    path: '/',
+    element: <ChatPage />,
+    children: [],
+    configure: {
+      verifToken: false,
+      role: ['user', 'administrator']
+    }
+  }
+];
+
